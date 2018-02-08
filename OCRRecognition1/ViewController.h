a87cb1ec56616d947ca6ccc48a31e07ff5e659fc
fred <fred@freddeMacBook-Pro.local>
2018-02-08 13:30:39 +0800

    Initial Commit

---
+++ OCRRecognition1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCRRecognition1
+//
+//  Created by fred on 2018/2/8.
+//  Copyright © 2018年 fred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
